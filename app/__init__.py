ea38d8c750ea3a65ec3e503013fb7858701457d7
Soren Thompson <soren.m.thompson.20@dartmouth.edu>
2019-05-16 15:19:06 -0400

    Workshop done

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask
+from . import db
+from . import auth
 
 def create_app(test_config=None):
     # create and configure the app
@@ -23,8 +25,13 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
-
+    # @app.route('/')
+    # def hello():
+    #     return 'Hello, World!'
+        
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
     return app
